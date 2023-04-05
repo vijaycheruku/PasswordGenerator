9f28936fdb37f529ef7d8304ad5f1b9c4fd09706
vijay <vijay.cheruku@live.com>
2023-04-05 15:07:54 +0800

    Code comments added

--- Develop/script.js
+++ Develop/script.js
@@ -10,35 +10,40 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add click event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 //generatepasssword function here
 function generatePassword() {
   let myFinalGeneratedPassword = "";
-
+  //Required Array Initialisations for conditions 
   let lowercaseletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   let uppercaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   let numerics = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let specialcharacters = ['@', '!', '#', '$', '_'];
   let eleSelArray = [];
 
+  // Calling passwordinput function to get no of characters
   let charactersRequired = passwordInput();
 
+  // If password validation is failed calling passwordinput function again to get valid input
   if (!charactersRequired) {
     passwordInput();
   }
 
+  // getting user inputs on password characters requirement
   let isLowercaseRequired = callPromptFunc("Does your password need lowercase letters(Y/N)")
   let isUppercaseRequired = callPromptFunc("Does your password need uppercase letters(Y/N)")
   let isSpecialCaseRequired = callPromptFunc("Does your password need special characters letters(Y/N)")
   let isNumericOk = callPromptFunc("Does your password need Numeric(y/n) ");
 
+  // Throwing error if no input is provided by user
   if (!isLowercaseRequired && !isUppercaseRequired && !isSpecialCaseRequired && !isNumericOk) {
     alert("Atleast one character type should be selected. Please try again");
   }
 
+  //Creating final concatinated array based on user provided inputs
   if (isLowercaseRequired) {
     eleSelArray = eleSelArray.concat(lowercaseletters);
   }
@@ -55,6 +60,7 @@ function generatePassword() {
     eleSelArray = eleSelArray.concat(specialcharacters)
   }
 
+  //For loop added to get a random character from the final array to form password
   for (let i = 0; i < charactersRequired; i++) {
     myFinalGeneratedPassword += eleSelArray[Math.floor(Math.random() * eleSelArray.length)];
   }
@@ -62,10 +68,11 @@ function generatePassword() {
   return myFinalGeneratedPassword;
 }
 
-
+// Function to get no. of characters required in pwd
 function passwordInput() {
   let userEnteredLenValue = prompt("Select a number of characters required in between 8 and 128");
 
+  // Validating if passwrd length entered is in between 8 and 128
   if (userEnteredLenValue >= 8 && userEnteredLenValue <= 128) {
     return userEnteredLenValue;
   } else {
@@ -74,7 +81,7 @@ function passwordInput() {
   }
 }
 
-
+//Prompt function added to take user inputs on Password Requirements
 function callPromptFunc(conditions) {
   let userEnteredValue = prompt(conditions);
   if (userEnteredValue == 'y' || userEnteredValue == 'Y') {
